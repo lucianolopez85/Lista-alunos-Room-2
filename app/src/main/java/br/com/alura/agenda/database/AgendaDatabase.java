896eca6d716d456ff5a408690d22175a89668f5e
lucianolopez85 <lopezz.lucio85@gmail.com>
2021-07-23 14:48:29 -0300

    first commit

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,30 @@
+package br.com.alura.agenda.database;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import android.content.Context;
+
+import br.com.alura.agenda.database.converter.ConversorCalendar;
+import br.com.alura.agenda.database.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+import static br.com.alura.agenda.database.AgendaMigrations.TODAS_MIGRATIONS;
+
+@Database(entities = {Aluno.class}, version = 4, exportSchema = false)
+@TypeConverters({ConversorCalendar.class})
+public abstract class AgendaDatabase extends RoomDatabase {
+
+    private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+    public abstract AlunoDAO getAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context) {
+        return Room
+                .databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
+                .allowMainThreadQueries()
+                .addMigrations(TODAS_MIGRATIONS)
+                .build();
+    }
+}
